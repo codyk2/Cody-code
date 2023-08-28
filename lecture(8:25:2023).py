eac75af3f926f063eb0a85570b7947b19cf8a21b
codyk2 <122074466+codyk2@users.noreply.github.com>
2023-08-28 10:58:16 -0500

    first commit

---
+++ lecture(8:25:2023).py
@@ -0,0 +1,25 @@
+'''
+Author: Cody Kandarian
+Assignment Title:
+Assignment Description:
+Due Date:
+Date Created:
+Date Last Modified:
+
+'''
+
+#wage = "don't"
+
+wage = 25
+hours = 17
+#print("My earnings per hour are: ", wage)
+
+#statement = "My earnings are: "
+#print(statement, wage)
+earning = wage * hours
+#print("What I make:", wage, "per hour, for,", hours.)
+print("Earning:", earning)
+print("wage:", wage)
+
+print(earning, end=' ')
+print(wage)
---
+++ program3.py
@@ -0,0 +1,22 @@
+''' 
+Author: Cody Kandarian
+Assignment Title: Bear
+Assignment Description: Printing to output images and input
+Due Date: 8/28/2023
+Date Created: 8/26/2023
+Date Last Modified: 8/28/2023
+'''
+#Data Abstraction
+#Input
+name = input(" Enter bear's name: ")
+
+#Process
+
+#Output
+print()
+print()
+print('ʕ•ᴥ•ʔ')
+print()
+print(' My', name, 'bear likes honey.')
+
+#Assumptions
